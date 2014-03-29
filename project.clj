1b81c9196d7d0a09691c6a903f48ee95fdd29239
Guillermo Winkler <guillermo.winkler@gmail.com>
2014-03-28 19:05:19 -0700

    byte-streams is not needed anymore
    Binary annotations are created directly from ByteBuffer

--- project.clj
+++ project.clj
@@ -4,11 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [thrift-clj "0.1.1"]
+                 [thrift-clj "0.2.1"]
                  [ch.qos.logback/logback-classic "1.0.13"]
                  [org.clojure/data.codec "0.1.0"]
                  [clj-scribe "0.3.1"]
-                 [byte-streams "0.1.6"] 
                  [clj-time "0.6.0"]]
   :test-paths ["test" "examples"]
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]
